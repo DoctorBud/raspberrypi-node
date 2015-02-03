ba9173323cfd6695d3aa1c0dff55b8379760e4ca
dkeith <dkeith@mindboxstudios.com>
2015-02-03 12:20:54 -0800

    clarified the state machine for RA

--- ricart-agrawala/server.js
+++ ricart-agrawala/server.js
@@ -1,55 +1,3 @@
-//
-// This file initializes the following services:
-//   - HTTP for the purpose of WebUI
-//   - HTTP for the purpose of REST API
-//   - Discovery
-//
-// This file is also responsible for maintaining the state of a Site for the purposes
-// of the algorithm. We will be using the terminology and description from:
-//    [Distributed Computing: Principles, Algorithms, and Systems](http://www.cs.uic.edu/~ajayk/Chapter9.pdf)
-// and:
-//    [WikiPedia](http://en.wikipedia.org/wiki/Ricart–Agrawala_algorithm)
-// as guidance in implementing the algorithm.
-//
-
-var myHost = null;
-var myPort = null;
-
-var participantsChosen = false;
-var participatingNodes = [];
-
-
-//
-// Synchronous logging to a shared log file
-//
-
-var fs = require('fs');
-var util = require('util');
-
-var syncLogFile = 'log.txt';
-function syncLog() {  // uses 'arguments'
-  var args = Array.prototype.slice.call(arguments, 0);
-  var data = '[' + myHost + ':' + myPort + ']\n  ' + util.format(args);
-  fs.appendFileSync(syncLogFile, data + '\n');
-  console.log(data);
-}
-
-
-//
-// Set up the WebUI/REST server
-//
-
-var Path = require('path');
-var Util = require('util');
-
-
-////////////////////////////////////////////////////////////////////////
-// Discovery and self-identification
-//
-var serverDiscovery = require('./server-discovery.js');
-
-
-
 ////////////////////////////////////////////////////////////////////////
 // Ricart-Agrawala algorithm
 //
@@ -58,33 +6,97 @@ var serverDiscovery = require('./server-discovery.js');
 //
 // I'm also having a Site's PID be it's 'host:port' string.
 //
+// This file initializes the following services:
+//   - HTTP for the purpose of WebUI
+//   - HTTP for the purpose of REST API
+//   - Discovery
+//
+// This file is also responsible for maintaining the state of a Site for the purposes
+// of the algorithm. We will be using the terminology and description from:
+//    [Distributed Computing: Principles, Algorithms, and Systems](http://www.cambridge.org/us/academic/subjects/engineering/communications-and-signal-processing/distributed-computing-principles-algorithms-and-systems)
+// as guidance in implementing the algorithm.
+//
+
+// Dependencies
+var Path = require('path');
+var Util = require('util');
+var Discovery = require('./server-discovery.js');
+var FS = require('fs');
+
+// Test Harness Configuration
+// Not part of the algorithm, but needed to set up the distributed environment
+// and participants list.
+
+var configDiscoveryDelay = 10000;
+var configNumTests = 3;
+var configGapTimeLow = 100;
+var configGapTimeHigh = 2000;
+var configWorkTimeLow = 2000;
+var configWorkTimeHigh = 5000;
+
+
+// All my State is living up here at the top of file because
+// I'm still in wild-editing mode and don't know where stuff will end up.
+// Eventually, I'll tight-scope everything.
+
+var myHost = null;
+var myPort = null;
+var myPID = null;
+
+var numTestsRemaining = 0;
 
 //
 // State vars for the RA Algorithm
 //
+var participantsChosen = false;
+var participatingNodes = null;  // List of PIDs discovered before Lockout
+                              // Each PID is a URI http://host:port
 var RD = [];  // Request-Deferred array (I'm just using a list of PIDs)
 var TS = 0;   // Lamport-style logical clock, incremented to T+1 upon a request with TS T.
-var numTests = 0;
 
 var entryREQUESTed = false;
 var numPendingREPLY = 0;
 
+var stateINIT = 0;
+var stateGAP = 1;
+var stateREQUEST = 2;
+var stateWORKING = 3;
+var state = stateINIT;
+
+
+function buildPID(host, port) {
+  return 'http://' + host + ':' + port;
+}
+
+//
+// Synchronous logging to a shared log file (log.txt)
+//
+
+var syncLogFile = 'log.txt';
+function syncLog() {  // uses 'arguments'
+  var args = Array.prototype.slice.call(arguments, 0);
+  var data = '[' + myPID + ']\n  ' + Util.format(args);
+  FS.appendFileSync(syncLogFile, data + '\n');
+  console.log(data);
+}
+
+
+// Random delay (ms) in interval [low,high]
 function randomDelay(low, high) {
   var result = Math.floor(low + Math.random() * (high - low));
-
   return result;
 }
 
 
 function workLeave() {
-  var workTime = randomDelay(1000, 5000);
+  var workTime = randomDelay(configWorkTimeLow, configWorkTimeHigh);
   syncLog('Work', workTime);
   setTimeout(
     function () {
       syncLog('Leave');
-      numTests--;
-      if (numTests > 0) {
-        delayEnterWorkLeave();
+      numTestsRemaining--;
+      if (numTestsRemaining > 0) {
+        enterStateGAP();
       }
       else {
         syncLog('Testing Complete');
@@ -97,7 +109,7 @@ var request = require('request');
 
 function handleRequestResponse(error, response, body) {
   if (error || response.statusCode != 200) {
-    syncLog('    handleREQUESTResponse ERROR', error, response, body);
+    syncLog('    handleRequestResponse ERROR', error, response, body);
   }
 }
 
@@ -105,8 +117,6 @@ function sendREQUEST() {
   syncLog('sendREQUEST');
   entryREQUESTed = true;
   numPendingREPLY = participatingNodes.length - 1;
-
-
 
   for (var i = 0; i < participatingNodes.length; ++i) {
     var site = participatingNodes[i];
@@ -161,8 +171,15 @@ function handleREPLY(msg) {
 }
 
 
-function enterWorkLeave() {
-  syncLog('enterWorkLeave');
+function enterStateREQUEST() {
+  syncLog('enterStateREQUEST');
+
+  //
+  // Prepare the RD array, which will begin accumulating deferred
+  // requests
+  // .... TBD
+
+  state = stateREQUEST;
 
   //
   // We can't do work until we Enter the CS
@@ -173,24 +190,30 @@ function enterWorkLeave() {
 
 
 
-function delayEnterWorkLeave() {
-  syncLog('delayEnterWorkLeave');
-  var delayTime = randomDelay(100, 2000);
+function enterStateGAP() {
+  syncLog('enterStateGAP');
+  var delayTime = randomDelay(configGapTimeLow, configGapTimeHigh);
   syncLog('delay', delayTime);
+  state = stateGAP;
 
   setTimeout(
     function () {
-        enterWorkLeave();
+        enterStateREQUEST();
     }, delayTime);
 }
 
-
-function testRA() {
-  syncLog('testRA');
-  numTests = 3;
-  delayEnterWorkLeave();
+//
+// We'll perform configNumTests sequences of:
+//    GAP (random delay before requesting anything)
+//    REQUEST
+//    WORK (after algorithm permits entry into CS, delay a random amount)
+//    LEAVE (and notify other participants).
+//
+function beginSimulation() {
+  syncLog('beginSimulation... configNumTests:', configNumTests);
+  numTestsRemaining = configNumTests;
+  enterStateGAP();
 }
-
 
 function participantsChanged(nodeList) {
   if (participantsChosen) {
@@ -206,8 +229,8 @@ function participantsChanged(nodeList) {
     participatingNodes = sortedList.map(
       function(element) {
         var advertisement = element.advertisement;
-        var url = 'http://' + advertisement.myHost + ':' + advertisement.myPort;
-        return url;
+        var pid = buildPID(advertisement.myHost, advertisement.myPort);
+        return pid;
       });
   }
 };
@@ -242,6 +265,7 @@ server.route({
         var context = {
             host:   myHost,
             port:   myPort,
+            pid:    myPID,
             nodes:  participatingNodes
         };
 
@@ -272,15 +296,12 @@ server.start(function () {
   var ip = require('ip');
   myHost = ip.address();
   myPort = server.info.port;
+  myPID = buildPID(myHost, myPort);
+  syncLog('Server started at: ' + myPID);
 
-  myURI = 'http://' + myHost + ':' + myPort;
+  syncLog('### Discovery Initiated for ', configDiscoveryDelay, 'ms');
 
-  syncLog('Server started at: ' + myURI);
-
-  var discoveryDelay = 10000;
-  syncLog('### Discovery Initiated for ', discoveryDelay, 'ms');
-
-  serverDiscovery.startDiscovery(myHost, myPort, participantsChanged);
+  Discovery.startDiscovery(myHost, myPort, participantsChanged);
 
   setTimeout(
     function () {
@@ -291,8 +312,8 @@ server.start(function () {
         syncLog('   [', i, '] ', participatingNodes[i]);
       }
 
-      testRA();
-    }, discoveryDelay ); // Wait 10secs to open all processes.
+      beginSimulation();
+    }, configDiscoveryDelay ); // Wait 10secs to open all processes.
 });
 
 
